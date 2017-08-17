49dee410ecf928a8c2f0aca3ac155df4534f73e7
MMoroney <email": null>
2017-08-17 14:51:42 -0500

    Seed data complete

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,9 +5,10 @@ require 'faker'
   User.new(username: Faker::RickAndMorty.character, email: Faker::Internet.safe_email, password_hash: Faker::Internet.password)
 end
 
-#make questions
+#make questions, each associated with a random user
 20.times do
-  Question.new(title: Faker::Hipster.sentence, explanation: Faker::Hipster.pararaph) #author_id?
+  author = User.all.shuffle.first
+  Question.new(title: Faker::Hipster.sentence, explanation: Faker::Hipster.pararaph, author_id: author.id)
 end
 
 
@@ -17,11 +18,12 @@ questions = Question.all
 #make comments to question
 questions.each do |question|
   5.times do
-    Comment.new(commentable_id: question.id, Faker::RickAndMorty.quote) #commentable_id correct? What's commentable type? author_id?
+    Comment.new(content: Faker::RickAndMorty.quote, author_id: question.author.id, commentable_id: question.id)
   end
 
 #make answers to question
   3.times do
-    Answer.new(content: Faker::Hipster.pararaph, question_id: question.id) #author_id?
+    author = User.all.shuffle.first
+    Answer.new(content: Faker::Hipster.pararaph, question_id: question.id, author_id: author.id)
   end
 end
