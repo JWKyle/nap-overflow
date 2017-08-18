9194651da55e1ad5673d6e12ff1d5d75cabe8a5c
MMoroney <email": null>
2017-08-18 12:06:03 -0500

    Add forms for making questions, answers, WIP for comment on answer form

---
+++ app/controllers/comments_controller.rb
@@ -0,0 +1,21 @@
+post '/answers/:answer_id/comments' do
+  @answer = Answer.find(params[:answer_id])
+  @comment = @answer.comments.new(params[:comment])
+
+  if @comment.save
+    redirect "/answers/#{@answer.id}"
+  else
+    erb :'/answers/_comment'
+  end
+end
+
+post '/questions/:question_id/comments' do
+  @question = Question.find(params[:question_id])
+  @comment = @question.comments.new(params[:comment])
+
+  if @comment.save
+    redirect "/questions/#{@question.id}"
+  else
+    erb :'/questions/_comment'
+  end
+end
